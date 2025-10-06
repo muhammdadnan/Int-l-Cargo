27bf28f45576c5136c8857b1acec6acc025ffc7b
Muhammadtalha678 <muhammadtalha0110@gmail.com>
2025-10-06 20:15:29 +0500

    add booking list to route

--- src/components/CsvModal.tsx
+++ src/components/CsvModal.tsx
@@ -60,7 +60,7 @@ const CsvModal = ({ setShowModal, branches, bookings }) => {
     console.log(bookingDate);
     
     return (
-   <div className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-40 z-50">
+   <div className="mt-20 fixed inset-0 flex items-center justify-center bg-black bg-opacity-40 z-50">
           <div className="bg-white rounded-lg shadow-lg w-96 p-6">
             <h2 className="text-xl font-bold mb-4 text-center">
               Export CSV Options
