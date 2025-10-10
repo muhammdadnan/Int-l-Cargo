7408d7b7719e23783862898dadd630bfd9b4da24
Muhammadtalha678 <muhammadtalha0110@gmail.com>
2025-10-10 14:02:17 +0500

    add cross button to clear earch bar

--- src/pages/bookingList.tsx
+++ src/pages/bookingList.tsx
@@ -79,9 +79,23 @@ const BookingList = () => {
   };
 
   // ✅ FILTERED BOOKINGS
-  const filteredBookings = bookings.filter(booking =>
-    booking.InvoiceNo?.toString().includes(searchQuery.trim())
+  // const filteredBookings = bookings.filter(booking =>
+  //   booking.InvoiceNo?.toString().includes(searchQuery.trim())
+  // );
+  const filteredBookings = bookings.filter((booking) => {
+  const q = searchQuery.trim().toLowerCase();
+
+  return (
+    booking.InvoiceNo?.toString().toLowerCase().includes(q) ||
+    booking.BiltyNo?.toString().toLowerCase().includes(q) ||
+    booking.SenderName?.toLowerCase().includes(q) ||
+    booking.ReceiverName?.toLowerCase().includes(q) ||
+    booking.SenderMobile?.toString().includes(q) ||
+    booking.ReceiverMobile1?.toString().includes(q) ||
+    booking.ReceiverMobile2?.toString().includes(q)
   );
+});
+
   // ✅ PAGINATION LOGIC
   const totalPages =
  itemsPerPage === "All"
@@ -129,14 +143,28 @@ const BookingList = () => {
       </h1>
 
       {/* ✅ SEARCH BAR */}
-      <div className="flex justify-center mb-4">
+      <div className="flex justify-center mb-4 relative w-1/2 mx-auto">
         <input
           type="text"
-          placeholder="Search by Invoice No..."
+          // placeholder="Search by Invoice No..."
+          placeholder="Search by Invoice No, Bilty No, Name or Phone..."
+
           value={searchQuery}
-          onChange={(e) =>{ setSearchQuery(e.target.value);setCurrentPage(1)}}
-          className="w-1/2 px-4 py-2 border border-gray-300 rounded shadow-sm focus:outline-none focus:ring-2 focus:ring-blue-500"
+          onChange={(e) =>{ 
+            setSearchQuery(e.target.value);
+            setCurrentPage(1)
+          }}
+          // className="w-1/2 px-4 py-2 border border-gray-300 rounded shadow-sm focus:outline-none focus:ring-2 focus:ring-blue-500"
+          className="w-full px-4 py-2 border border-gray-300 rounded shadow-sm focus:outline-none focus:ring-2 focus:ring-blue-500 pr-10"
         />
+        {searchQuery && (
+    <button
+      onClick={() => setSearchQuery("")}
+      className="absolute right-3 top-1/2 transform -translate-y-1/2 text-gray-500 hover:text-gray-700 focus:outline-none"
+    >
+      ✕
+    </button>
+  )}
       </div>
       
               {/* ✅ Per Page Dropdown */}
